05a8011cfca77ec606da3f85a00b46e13f0871ef
binhtdnd <binhtdnd@gmail.com>
2024-08-03 23:09:55 +0700

    connect db

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,36 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+// 
+
+const MONGODB_URI = 'mongodb+srv://binhtdnd:@cluster0-trello.aikvlar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-Trello'
+const DATABASE_NAME = 'data-trello'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  // gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // sau await , sau khi ket noi thanh cong, gan bien trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+//tận dụng lại biến trelloDatabaseInstance, gọi lại ở nơi khác để sử dụng nếu cần
+//neu chua ket noi thi hay ket noi
+export const GET_DB = () => {
+  // throw giong return , tra ra thong bao ket noi
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  // neu ket noi dc tra ra Instance
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,28 +6,44 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+    res.end('<h1>Hello world</h1>')
+  })
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Binh, I am running at ${hostname}:${port}/`)
+  })
+}
+
+// chi khi ket noi den database thanh cong moi start server backend
+console.log('connecting...')
+CONNECT_DB()
+  .then(() => console.log('connected to Mdb cluod success'))
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
+
+// (async () => {
+//   try {
+//     console.log('connecting...')
+//     await CONNECT_DB()
+//     console.log('connected to Mdb cluod success')
+//     START_SERVER()
+//   } catch (error) {
+//     console.error(error)
+//     process.exit(0)
+//   }
+// })()
