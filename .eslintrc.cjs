5e202cb605bcf37e53f3c3c92a14b4c0f5ce98e8
binhtdnd <binhtdnd@gmail.com>
2024-08-10 16:33:30 +0700

    CARD_COLLECTION_NAME & SCHEME on board-column-card Model

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,7 +35,7 @@ module.exports = {
     'linebreak-style': 0,
     'no-unexpected-multiline': 'warn',
     'keyword-spacing': 1,
-    'comma-dangle': 1,
+    'comma-dangle': 0, //dau , o phan tu cuoi cung trong {}
     'comma-spacing': 1,
     'arrow-spacing': 1
   }
