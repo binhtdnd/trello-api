c569bb3c74db3e064dff3b369de984d6c1dd6f63
binhtdnd <binhtdnd@gmail.com>
2024-09-05 22:18:17 +0700

    keo card trong cung column

--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -21,6 +21,36 @@ const createNew = async (req, res, next) => {
 
 }
 
+
+const update = async (req, res, next) => {
+
+  const correctCondition = Joi.object({
+    // di chuyen? card cung column thi ko update board ID
+    // boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+    title: Joi.string().min(3).max(50).trim().strict(),
+    cardOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    )
+
+  })
+  try {
+    //abortEarly: false de? truong hop co nhieu loi validation thi tra ve tat ca cac loi, ko thi chi tra 1 loi dau tien
+    //doi voi turong hop update cho phep allowUnknown: true de cho phep day len 1 truong` chua dinhj nghia (unknown)
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true,
+    })
+    next()
+
+  } catch (error) {
+    const errorMessege = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessege)
+    next(customError)
+  }
+
+
+}
 export const columnValidation = {
-  createNew
+  createNew,
+  update
 }
